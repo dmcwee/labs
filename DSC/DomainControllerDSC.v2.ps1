e529cea7c64a2c2c64e8dfa941ad164b4abe33ad
David McWee <dmcwee@microsoft.com>
2022-11-14 09:03:44 -0500

    test v2 of dsc

---
+++ DSC/DomainControllerDSC.v2.ps1
@@ -0,0 +1,57 @@
+Configuration Main
+{
+
+	$adminAccount = Get-AutomationPSCredential -Name "DomainAdmin"
+	$domainName = Get-AutomationVariable -Name "DomainName"
+
+	Import-DscResource -ModuleName PSDesiredStateConfiguration
+	Import-DscResource -ModuleName ComputerManagementDsc
+	Import-DscResource -ModuleName ActiveDirectoryDsc
+
+	<#
+	[PSCredential]$Creds = New-Object -TypeName PSCredential ($adminAccount.UserName, $adminAccount.Password)
+	$OUPath = ($domainName.split('.') | ForEach-Object { "DC=$_" }) -join ','
+	$UserPath = "OU=LabUsers," + $OUPath
+	#>
+		
+	Node localhost
+	{
+		Log WriteOU {
+			Message = "The OUPath is " + $OUPath
+		}
+
+		Log WriteUserPath {
+			Message = "The Userath is " + $UserPath
+		}
+
+		WindowsFeature ADDomainServices {
+			Name="AD-Domain-Services"
+			Ensure="Present"
+	 	}
+
+		WindowsFeature DNS {
+			Name="DNS"
+			Ensure="Present"
+			IncludeAllSubFeature = $true
+		}
+
+		WindowsFeature ADTools {
+			Name="RSAT-AD-Tools"
+			Ensure = "Present"
+			IncludeAllSubFeature = $true
+		}
+
+		ADDomain CreateDomain {
+			DomainName = $domainName
+            Credential = $Creds
+			SafemodeAdministratorPassword = $Creds
+            DependsOn = "[WindowsFeature]ADDomainServices"
+		}
+
+		WaitForADDomain CreatedDomain {
+			DomainName = $domainName
+			RestartCount = 2
+			WaitTimeout = 600
+			DependsOn = "[xADDomain]CreateDomain"
+		}
+}
