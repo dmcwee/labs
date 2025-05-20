a4aaec7e2d9160e230d4ec3ac93d439aa1ef6a32
David McWee <dmcwee@microsoft.com>
2025-05-20 11:57:59 -0400

    single dsc file.

--- Common/DSC/DcDesiredState.ps1
+++ Common/DSC/DcDesiredState.ps1
@@ -1,3 +1,50 @@
+Configuration Prerequisites {
+	
+	Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    Import-DScResource -ModuleName 'ComputerManagementDsc'
+
+    Node localhost {
+        WindowsFeature ADDomainServices {
+			Name="AD-Domain-Services"
+			Ensure="Present"
+	 	}
+
+		WindowsFeature DNS {
+			Name="DNS"
+			Ensure="Present"
+			IncludeAllSubFeature = $true
+		}
+
+		WindowsFeature DNSTools {
+			Name="RSAT-DNS-Server"
+			Ensure="Present"
+			IncludeAllSubFeature = $true
+		}
+
+		WindowsFeature ADTools {
+			Name="RSAT-AD-Tools"
+			Ensure = "Present"
+			IncludeAllSubFeature = $true
+		}
+
+        Script ActiveDirectoryDscResource {
+            GetScript = {
+				$module = Import-Module -Name ActiveDirectoryDsc
+                return $module
+            }
+            SetScript = {
+                # Install-Module -Name ActiveDirectoryDsc -AllowClobber -Force -SkipPublisherCheck
+				Install-Module -Name ActiveDirectoryDsc -Repository PSGallery -Force
+            }
+            TestScript = {
+				$import = Import-Module -Name ActiveDirectoryDsc
+				return $null -ne $import
+            }
+        }
+
+    }
+}
+
 Configuration Main
 {
 	Param ( 
